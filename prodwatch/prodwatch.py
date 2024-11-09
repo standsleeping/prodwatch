6b1b9b7124da411376912cc3926c0e5c6ec6bc24
standsleeping <standsleeping@gmail.com>
2024-11-09 12:49:33 -0600

    Ignore imports in hidden directories

--- prodwatch/prodwatch.py
+++ prodwatch/prodwatch.py
@@ -69,6 +69,7 @@ def start_ipc_server():
 def import_user_modules():
     project_root = os.getcwd()
     for root, dirs, files in os.walk(project_root):
+        dirs[:] = [d for d in dirs if not d.startswith('.')]
         for file in files:
             if file.endswith(".py"):
                 module_path = os.path.relpath(os.path.join(root, file), project_root)
@@ -78,6 +79,8 @@ def import_user_modules():
                     importlib.import_module(module_name)
                 except ImportError:
                     print(f"Failed to import {module_name}")
+                except Exception as e:
+                    print(f"Error attempting import of {module_name}: {e}")
 
 
 def add_project_to_path():
